fd1221b7f6cb0c314648f2b598de779092798b06
Christopher Poon <christopherpoonc@gmail.com>
2024-12-11 23:19:03 -0500

    update docs
    Signed-off-by: Christopher Poon <christopherpoonc@gmail.com>

---
+++ docs/source/conf.py
@@ -0,0 +1,36 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Quantum Education Modules'
+copyright = '2024, Quantum Education Modules'
+author = 'Quantum Education Modules'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.doctest",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.githubpages",
+    'sphinx_rtd_theme'
+]
+
+templates_path = ['_templates']
+
+master_doc = "index"
+
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+html_theme = "sphinx_rtd_theme"
+
+html_static_path = ['_static']
---
+++ docs/source/index.rst
@@ -0,0 +1,17 @@
+.. CSCI4961_labs_projects documentation master file, created by
+   sphinx-quickstart on Wed Dec 11 22:26:35 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+CSCI4961_labs_projects documentation
+====================================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
